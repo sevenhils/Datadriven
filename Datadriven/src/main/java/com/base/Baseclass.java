72958ae82561f64606f0af1db2c356c2a6dc72b2
ADMIN <ADMIN@LAPTOP-PM0UH2ND>
2021-09-29 15:23:50 +0530

    second commit

--- Datadriven/src/main/java/com/base/Baseclass.java
+++ Datadriven/src/main/java/com/base/Baseclass.java
@@ -2,6 +2,9 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 
@@ -9,53 +12,45 @@ import com.utilites.Excellibrary;
   
 public class Baseclass {
 	public WebDriver driver;
-	Excellibrary obj=new Excellibrary("C:\\Users\\ADMIN\\eclipse-workspace\\Datadriven\\Testdata\\user.xlsx");
+	 Excellibrary obj= new Excellibrary("C:\\Users\\ADMIN\\git\\Datadriven\\Datadriven\\Testdata\\user.xlsx");
+
 @BeforeMethod
 	public void setup() {
-	System.setProperty("webdriver.chrome.driver", "E:\\Chrome driver\\chromedriver.exe");
-	 driver=new ChromeDriver();
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "E:\\Chrome driver\\chromedriver.exe"); driver=new ChromeDriver();
+		 */
+	System.setProperty("webdriver.gecko.driver","E:\\firefox\\geckodriver.exe");
+	driver=new FirefoxDriver();	
+
 	driver.manage().window().maximize();
 	driver.get("https://opensource-demo.orangehrmlive.com/");
 	
 		
 	}
+@AfterMethod
 	
 public void teardown() {
 	driver.quit();
-	
 }
-@DataProvider(name="Credentials")
-	public Object[][] getdata() {
-		Object[][] data=new Object[3][2];
-		
-		data[0][0]="Admin";
-		data[0][1]="admin123";
-
-		data[1][0]="Admin1";
-		data[1][1]="admin123";
-
-		data[2][0]="Admin";
-		data[2][1]="admin143";
-		return data;
-		}
- @DataProvider(name="logincredentials")
-			public Object[][] getexceldata() {
-	 //total Row count
-			int rows	=obj.getRowCount("data");
-			// total column count
-			int coloumns=obj.getColumnCount("data");
-			int actrows=rows-1;
-			Object[][] data=new Object[actrows][coloumns];
-			
-			for(int i=0;i<actrows;i++) {
-				for(int j=0;j<coloumns;j++) {
-					data[i][j]=obj.getCellData("data", j, i+2);
-				}
-			}
-			return data;
-			
-			}
-	}
+	@DataProvider(name ="logincredentials")
+	 public Object[][] getExcelData() {
+	  //Totals rows count
+	  int rows=obj.getRowCount("data");
+	  //Total Columns
+	  int column=obj.getColumnCount("data");
+	  int actRows=rows-1;
+	  
+	  Object[][] data= new Object[actRows][column];
+	  
+	  for(int i=0;i<actRows;i++) {
+	   for(int j=0; j<column;j++) {
+	    data[i][j]=obj.getCellData("data", j, i+2);
+	   }
+	  }
+	  return data;
+}
+ 	}
 		
 		
 	
---
+++ Datadriven/src/test/java/sendmail/sendmail.java
@@ -0,0 +1,29 @@
+package sendmail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+public class sendmail {
+
+	public static void main(String[] args) throws EmailException {
+		// TODO Auto-generated method stub
+
+		Email email= new SimpleEmail();
+		email.setHostName("smtp.yahoo.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator("akulavenkatesh666", "venky@789"));
+		email.setSSLOnConnect(true);
+		email.setFrom("akulavenkatesh666@yahoo.com");
+			email.setSubject("Test Mail");
+			email.setMsg("Test passed");
+			email.addTo("akulavenkatesh666@outlook.com");
+			email.send();
+			
+		
+		
+		
+	}
+
+}
